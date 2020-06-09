850c85ff8ff763875eb0ad5b9f31ccb1ec17e0c5
Yunus Emre Tatar <emre.tatar@sestek.com>
2020-06-09 10:54:32 +0300

    first commit

---
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-lib',
+  template: `
+    <p>
+      Hello world
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
